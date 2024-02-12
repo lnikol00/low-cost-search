feeb8bbecd26a56617e89c07a7e3073c620c3932
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-02-12 10:36:40 +0100

    Creating dotnet project

---
+++ backend/TravelApp/TravelApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using TravelApp.Models;
+using TravelApp.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddSingleton<IAmadeusRepository, AmadeusRepository>();
+
+builder.Services.AddCors(p => p.AddPolicy("cors_policy_allow_all", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
