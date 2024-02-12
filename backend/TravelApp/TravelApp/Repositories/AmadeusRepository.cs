c5ecbebaeeb9a422d89cc5f213a439bfeccad14c
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-02-12 10:37:31 +0100

    Adding controller and repository

---
+++ backend/TravelApp/TravelApp/Repositories/AmadeusRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace TravelApp.Repositories
+{
+    public class AmadeusRepository : IAmadeusRepository
+    {
+        public async Task<string> GetFlightAsync(string origin, string destination, string departureDate, string returnDate, string currency, int passengers, string token)
+        {
+            string url = $"https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode={origin}&destinationLocationCode={destination}&departureDate={departureDate}&adults={passengers}&currencyCode={currency}";
+
+            if (!string.IsNullOrEmpty(returnDate))
+            {
+                url += $"$returnDate={returnDate}";
+            }
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            using var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            return "";
+        }
+    }
+}
---
+++ backend/TravelApp/TravelApp/Repositories/IAmadeusRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace TravelApp.Repositories
+{
+    public interface IAmadeusRepository
+    {
+        Task<string> GetFlightAsync(string origin, string destination, string departureDate, string returnDate, string currency, int passengers, string token);
+    }
+}
